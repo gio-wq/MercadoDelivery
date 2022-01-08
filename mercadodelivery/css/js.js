3fdd231c8ad5642472fdf4c42254302e3a949ea9
Giovanna Andrade <66229855+gio-wq@users.noreply.github.com>
2022-01-07 21:45:53 -0300

    Delete mercadodelivery directory

--- mercadodelivery/css/js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
- var botao = $('.bt');
- var dropDown = $('.ulprod');    
-
-    botao.on('click', function(event){
-        dropDown.stop(true,true).slideToggle();
-        event.stopPropagation();
-    });
-});
